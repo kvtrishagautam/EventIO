06d8fd7b641c39a5feb30a3baa6c04bea84b3c2e
k-h-aysha <ayshaharis144@gmail.com>
2024-06-18 19:10:35 +0530

    create event: completed

--- controllers/organizer.js
+++ controllers/organizer.js
@@ -7,8 +7,9 @@ module.exports = {
         try {
             // Fetch the all c-event id from the table userinfo
             let { data: cEvents, error } = await supabase
-                .from('user_info') 
-                .select('c_events');
+                .from('user_info')
+                .select('c_events')
+                .eq('user_id', '1fd3c2b2-86d9-4baa-8aa4-748432d209db');
 
             if (error) {
                 console.error('Supabase error:', error.message);
@@ -37,26 +38,28 @@ module.exports = {
                         console.error('Supabase search details:', error.details);
                         console.error('Supabase search hint:', error.hint);
                     } else {
-
-                        cResults.push( c_data );
+                        cResults.push(c_data);
                     }
                 }
             }
             console.log(cResults)
-            res.render('./organizer/conductedEvents', { title: 'Conducted Events', c_events : cResults  });
+            res.render('./organizer/conductedEvents', { title: 'Conducted Events', c_events: cResults });
         } catch (err) {
             console.error('Error fetching specific column:', err.message);
             res.status(500).send('Internal Server Error');
         }
     },
 
-    getAttendedEvents: async(req, res) => {
+    getAttendedEvents: async (req, res) => {
         try {
+
             // Fetch the all c-event id from the table userinfo
             let { data: aEvents, error } = await supabase
-                .from('user_info') 
-                .select('a_events');
-
+                .from('user_info')
+                .select('a_events')
+                .eq('user_id', '1fd3c2b2-86d9-4baa-8aa4-748432d209db');
+            console.log('hello')
+            console.log(aEvents)
             if (error) {
                 console.error('Supabase error:', error.message);
                 console.error('Supabase details:', error.details);
@@ -70,30 +73,87 @@ module.exports = {
 
             // Loop through all c_events data and find matching values in event table
             for (let item of aEvents) {
+                console.log(aEvents)
                 let { a_events: arrayofAE } = item;
+                console.log(arrayofAE)
                 for (let value of arrayofAE) {
+                    console.log(value)
 
                     let { data: a_data, error } = await supabase
                         .from('event')
                         .select('event_id,title,desc,start_day,expired')
-
                         .eq('event_id', value);
 
+                    console.log(a_data)
                     if (error) {
                         console.error('Supabase search error:', error.message);
                         console.error('Supabase search details:', error.details);
                         console.error('Supabase search hint:', error.hint);
                     } else {
-
-                        aResults.push( a_data );
+                        aResults.push(a_data);
                     }
                 }
             }
-            console.log(aResults)
-            res.render('./organizer/attendedEvents', { title: 'Attended Events', a_events : aResults  });
+            // console.log(aResults)
+            res.render('./organizer/attendedEvents', { title: 'Attended Events', a_events: aResults });
         } catch (err) {
             console.error('Error fetching specific column:', err.message);
             res.status(500).send('Internal Server Error');
         }
-        },
+    },
+
+    getCreateEvents: async (req, res) => {
+        res.render('./organizer/create-events', { title: 'Create Events' });
+
+    },
+
+    postCreateEvents: async (req, res) => {
+        // const { column1, column2 } = req.body;
+
+        let { data, error } = await supabase
+            .from('event')
+            .insert([
+                { title: req.body.title, desc: req.body.desc, venue: req.body.venue, start_time: req.body.start_time, end_time: req.body.end_time, start_day: req.body.start_day, end_day: req.body.end_day, org_id: 'f11ae58a-3e12-47d7-92aa-77bed087e5bb' }
+            ]);
+
+
+        let { data: event, error1 } = await supabase
+            .from('event')
+            .select('created_at,event_id')
+            .order('created_at', { ascending: false })
+            .limit(1);
+        console.log(event[0].event_id)
+
+
+        let { data: user_info, error3 } = await supabase
+            .from('user_info')
+            .select('c_events')
+            .eq('org_id', 'f11ae58a-3e12-47d7-92aa-77bed087e5bb');
+        console.log(user_info[0].c_events)
+
+
+        const updatedEvents = [...user_info[0].c_events, event[0].event_id];
+        console.log(updatedEvents)
+
+        let { data: updatedC_events, error4 } = await supabase
+            .from('user_info')
+            .update({ c_events: updatedEvents })
+            .eq('org_id', 'f11ae58a-3e12-47d7-92aa-77bed087e5bb')
+
+        if (error4) {
+            console.error('Supabase error:', error.message);
+            console.error('Supabase details:', error.details);
+            console.error('Supabase hint:', error.hint);
+            res.status(500).send('Error inserting data');
+        }
+        if (error) {
+            console.error('Supabase error:', error.message);
+            console.error('Supabase details:', error.details);
+            console.error('Supabase hint:', error.hint);
+            res.status(500).send('Error inserting data');
+        } else {
+            res.send('Data inserted successfully');
+        }
+    },
+
 }
--- routes/organizer.js
+++ routes/organizer.js
@@ -1,6 +1,6 @@
 var express = require('express');
 const { getHomePage } = require('../controllers/user');
-const { getConductedEvents,getAttendedEvents } = require('../controllers/organizer');
+const { getConductedEvents,getAttendedEvents,getCreateEvents,postCreateEvents } = require('../controllers/organizer');
 var router = express.Router();
 
 router.get('/',getHomePage);
@@ -9,5 +9,8 @@ router.get('/conducted-events', getConductedEvents);
 
 router.get('/attended-events', getAttendedEvents);
 
+router.get('/create-events', getCreateEvents);
+
+router.post('/create-events', postCreateEvents);
 
 module.exports = router;
