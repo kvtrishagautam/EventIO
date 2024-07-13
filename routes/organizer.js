66cd535363fa29f086e08f763e68c09fc04e66ff
Trisha <kvtrishagautam@gamil.com>
2024-07-13 16:32:13 +0530

    Done

--- routes/organizer.js
+++ routes/organizer.js
@@ -1,27 +1,27 @@
 var express = require('express');
 const { getHomePage } = require('../controllers/user');
 const { getConductedEvents,checkOrganizerExist,postCreateEvents, getCreateEvents,toStudentMode,postOrganizer} = require('../controllers/organizer');
-const {checkOrg} = require('../middlewares/checkOrg');
-const {checkLogedIn} = require('../middlewares/checkLogedIn');
+const checkOrg = require('../middlewares/checkOrg');
+const checkLogedIn = require('../middlewares/checkLogedIn');
 var router = express.Router();
-const {checkAccCreated} = require('../middlewares/checkAccCreated');
+const checkAccCreated = require('../middlewares/checkAccCreated');
 const {getOrganizerDash} = require('../controllers/organizer');
 
-router.get('/dashboard',getOrganizerDash)
+router.get('/dashboard',checkLogedIn,getOrganizerDash)
 
 router.get('/',getHomePage);
 
-router.get('/conducted-events', getConductedEvents);
+router.get('/conducted-events',checkLogedIn, getConductedEvents);
 
-router.get('/check-org', checkOrganizerExist);
+router.get('/check-org',checkLogedIn, checkOrganizerExist);
 
 router.post('/check-org', postOrganizer);
 
-router.get('/check-stud', toStudentMode);
+router.get('/check-stud',checkLogedIn, toStudentMode);
 
 router.get('/create-events',checkAccCreated, getCreateEvents);
 
-router.post('/create-events', postCreateEvents);
+router.post('/create-events',postCreateEvents);
 
 
 
